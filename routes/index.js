34bb12a4e98fd006354bb6146ae78cef8c0e3f0d
g-mac <simonmayrshofer@hotmail.com>
2017-10-23 13:39:42 +0200

    new route to helloworld

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
+});
+
+/* GET Hello World page. */
+router.get('/helloworld', function (req, res) {
+    res.render('helloworld', {title: 'Hello, World!'});
 });
 
 module.exports = router;
