fe3c9ab502c98fa9028d437280e51b34d8c1f8c0
g-mac <simonmayrshofer@hotmail.com>
2017-12-19 00:43:57 +0100

    implemented different kinds of approached for the experiment

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,66 +1,14 @@
-var count = 0;
-var msecsFirst = 0;
-var msecsPrevious = 0;
-var alternate = false;
+
 var lat = 0;
 var long = 0;
 var accuracy = 0;
-
-function ResetCount() {
-    count = 0;
-    document.TAP_DISPLAY.T_AVG.value = "";
-    document.TAP_DISPLAY.T_TAP.value = "";
-    document.TAP_DISPLAY.T_RESET.blur();
-}
-
-function TapForBPM(e) {
-    e.value
-    document.TAP_DISPLAY.T_WAIT.blur();
-    timeSeconds = new Date;
-    msecs = timeSeconds.getTime();
-
-    document.getElementById("key_pressed").innerHTML = String.fromCharCode(e.which)
-
-    if (alternate) {
-//                document.getElementById("dot1").src = "pulse1.gif"
-//                document.getElementById("dot2").src = "pulse.gif"
-        document.getElementById("dot1").style.visibility = "visible"
-        document.getElementById("dot2").style.visibility = "hidden"
-        alternate = false
-    } else {
-        document.getElementById("dot1").style.visibility = "hidden"
-        document.getElementById("dot2").style.visibility = "visible"
-        alternate = true
-    }
-
-    if ((msecs - msecsPrevious) > 1000 * document.TAP_DISPLAY.T_WAIT.value) {
-        count = 0;
-    }
-
-    if (count == 0) {
-        document.TAP_DISPLAY.T_AVG.value = "First Beat";
-        document.TAP_DISPLAY.T_TAP.value = "First Beat";
-        msecsFirst = msecs;
-        count = 1;
-    }
-    else {
-        bpmAvg = 60000 * count / (msecs - msecsFirst);
-        document.TAP_DISPLAY.T_AVG.value = Math.round(bpmAvg * 100) / 100;
-        document.TAP_DISPLAY.T_WHOLE.value = Math.round(bpmAvg);
-        count++;
-        document.TAP_DISPLAY.T_TAP.value = count;
-    }
-    msecsPrevious = msecs;
-    return true;
-}
-
-document.onkeypress = TapForBPM;
+var bpmAvg = 0;
 
 function postResult() {
     if (bpmAvg < 1)
         return
 
-    document.getElementById("submit_feedback").innerHTML = "sending...";
+    // document.getElementById("submit_feedback").innerHTML = "sending...";
 //            document.submit_feedback.value = "sending...";
 
     var newResult = {
@@ -83,12 +31,12 @@ function postResult() {
 //                document.getElementById("submit_feedback").innerHTML = "RESPONSE:";
         // Check for successful (blank) response
         if (response.msg === '') {
-            document.getElementById("submit_feedback").innerHTML = "SUCCESS";
+            // document.getElementById("submit_feedback").innerHTML = "SUCCESS";
             // Clear the form inputs
 //                    $('#addUser fieldset input').val('');
         } else {
             // If something goes wrong, alert the error message that our service returned
-            document.getElementById("submit_feedback").innerHTML = "FAILURE: " + response.msg;
+            // document.getElementById("submit_feedback").innerHTML = "FAILURE: " + response.msg;
 //                    document.getElementById("submit_feedback").innerHTML = "FAILURE: " + response.message;
 //                    alert('Error: ' + response.msg);
         }
@@ -109,6 +57,7 @@ function load_welcome() {
 }
 
 function load_experiment() {
+    bpmAvg = 0;
     $("#content").load("experiment.html");
 }
 
