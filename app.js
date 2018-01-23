81e961f04ca36649ab294c5eb0dbe082ad65ceeb
g-mac <simonmayrshofer@hotmail.com>
2018-01-23 01:26:52 +0100

    started finalisation of first version of welcome and prequestion pages

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 // Make our db accessible to our router
 app.use(function(req,res,next){
